8c838d4c3de53b5cd52331dd33bac7526e572e95
ADITYA RAJANI <86645931+rajaniaditya@users.noreply.github.com>
2024-09-03 11:22:07 +0530

    Add files via upload

---
+++ PR-09 Single-dimensional arrays/PR9.C
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    int arr[5] = {1, 2, 3, 4, 5};
+
+    // Accessing array elements
+    for (int i = 0; i < 5; i++) {
+        printf("Element at index %d: %d\n", i, arr[i]);
+    }
+    
+    return 0;
+}
